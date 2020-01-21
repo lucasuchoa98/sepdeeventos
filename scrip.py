6acbd7f579d57ea3e4d5033f0451877083f7c8a6
lucasuchoa98 <38196129+lucasuchoa98@users.noreply.github.com>
2020-01-21 09:09:23 -0300

    Add files via upload

--- scrip.py
+++ scrip.py
@@ -7,9 +7,12 @@ Created on Thu Sep 26 10:31:07 2019
 
 import pandas as pd
 import numpy as np
+import plotly as pl
+import plotly.express as px
 from datetime import datetime
 from datetime import time
 from datetime import timedelta
+
 
 arq = r"C:\Users\Lucas\Desktop\PIBIC 19_20\dados\Plu.F11.08.2019.xlsx"
 
@@ -22,6 +25,13 @@ d = df[1]
 
 class Evento:
     def __init__(self, deltat,deltae,ptot, imed):
+        
+        """
+        Essa funcao define os parametros para definicao de um evento
+        deltae e o tempo entre eventos em minutos,
+        ptot e a precipitacao total de um evento em milimetros,
+        imed e a intensidade media de um evento em milimetros por hora"""
+        
         self.deltat = timedelta(minutes = deltat)
         self.deltae = timedelta(minutes = deltae)
         self.ptot = float(ptot)
@@ -34,6 +44,11 @@ class Evento:
         df['data'] = lista
     
     def definindo_eventos(self):
+        
+        """
+        Essa funcao define os eventos com duracao menor que o deltae
+        """
+        
         aux = 0
         eventos = list()
         df1 = list()
@@ -53,17 +68,83 @@ class Evento:
                 eventos.append(df1)
                 df1 = list()
                 aux+=1
-        dframe = {i: pd.DataFrame(eventos[i], columns = ['data','pre']) for i in range(len(eventos))}
-        
-        
+        dframe = {i: pd.DataFrame(eventos[i], columns = ['data','pre']) for i in range(len(eventos))}        
         return dframe
-          
-class Status:
-    def __init__(self, evento):
-        self.evento = evento
-
+        
+    def sel_eventos(self,eventos):
+        
+        """
+        Essa funcao define os eventos com relacao a ptot e a imed
+        """
+        
+        for i in range(len(eventos)):
+            if eventos[i]['pre'].sum() <self.ptot:
+                eventos.pop(i)
+        for j in eventos.copy().keys():
+            self.timedelta = eventos[j]['data'].tail(1)[len(eventos[j])-1] - eventos[j]['data'][0]
+            if ((eventos[j]['pre'].sum()*3600)/self.timedelta.total_seconds()) < self.imed:
+                eventos.pop(j)
+        return eventos
+    
+    """def resumo(self, eventos):
+        print("Estatistica de cada evento\n ------------------------")
+        prec = list()
+        dur = list()
+        inte = list()
+        aux = 0
+        
+        for i in eventos.keys():
+            prec.append(eventos[i]['pre'].sum())
+            timedelta = eventos[i]['data'].tail(1)[len(eventos[i])-1] - eventos[i]['data'][0]
+            dur.append(timedelta.total_seconds())
+            inte.append(prec[aux]*60/(dur[aux]))
+            aux+=1
+        
+        print("Evento   Precipitação(mm)     Duração(min)    Itensidade Média(mm/h)")
+        for j,k in zip(range(aux-1),eventos.keys()):
+            print("%d    %.1f       %.0f      %.0f" %(k,prec[aux],dux[aux],inte[aux]))
+    """    
+        
+            
+            
+        
+class Discretizar:
+    def __init__(self, eventos):
+        self.eventos = eventos
+    
+    def discretizando(self,tempo_disc):
+        eventos_disc = list()
+        lista = list()
+        aux = 0
+        prec_acumulada = 0
+        for i in self.eventos.keys():
+            print("novo evento")
+            starting = self.eventos[i]['data'][0]
+            ending = self.eventos[i]['data'].tail(1)[len(eventos[i])-1]
+            date_range = pd.date_range(start = starting, end = ending, freq = '60min' )
+                
+            for j in self.eventos[i]['data']:
+                
+                try:
+                    date_r = date_range[aux].to_pydatetime() 
+                    date_j = j.to_pydatetime()
+                
+                    if date_r >= date_j:                       
+                        prec_acumulada += 0.2
+                    else:
+                        lista.append([date_r,prec_acumulada])
+                        print(lista)
+                        prec_acumulada = 0
+                        aux += 1
+                except:                        
+                    lista = list()                            
+                    prec_acumulada = 0
+                    aux = 0
+            eventos_disc.append(lista)
+        dframe = {k: pd.DataFrame(eventos_disc[k], columns = ['data','prec']) for k in range(len(eventos_disc))}
+            
+        return dframe
+            
 eventos = Evento(5,60,5,3).definindo_eventos()
-
-
-
-
+eventos = Evento(5,60,5,3).sel_eventos(eventos)
+teste = Discretizar(eventos).discretizando(5)
